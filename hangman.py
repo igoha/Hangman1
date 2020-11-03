672b855ccaeadb668d5af2e832a245fb59274656
igoha <igor_hajko@yahoo.com>
2020-11-03 23:32:51 +0100

    Initial commit

--- hangman.py
+++ hangman.py
@@ -4,8 +4,9 @@ import random
 random.seed()
 word_list = ['python', 'java', 'kotlin', 'javascript']
 print("H A N G M A N")
-word = input("Guess the word: ")
-r_word = random.choice(word_list)
+r_word = random.choice(word_list)  # random worl from word_list
+g_word = r_word[0:3] + "-" * (len(r_word) - 3)  # generated word first 3 letter from r_wordl + "-"
+word = input(f"Guess the word {g_word}: ")
 if word == r_word:
     print("You survived!")
 else:
