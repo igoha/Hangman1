eb1a2400efa1832b11ad1541675c19d3dac591ad
igoha <igor_hajko@yahoo.com>
2020-11-04 17:56:28 +0100

    vymayane stare veci

--- hangman.py
+++ hangman.py
@@ -1,15 +1,9 @@
 # hangman game
 import random
 
-
+letters = set()
 random.seed()
 word_list = ['python', 'java', 'kotlin', 'javascript']
+random_word = random.choice(word_list)  # random word from word_list
+# g_word = r_word[0:3] + "-" * (len(r_word) - 3)  # generated word first 3 letter from r_word + "-"
 print("H A N G M A N")
-r_word = random.choice(word_list)  # random word from word_list
-g_word = r_word[0:3] + "-" * (len(r_word) - 3)  # generated word first 3 letter from r_word + "-"
-word = input(f"Guess the word {g_word}: ")
-if word == r_word:
-    print("You survived!")
-else:
-    print("You lost!")
-# poynamka na konci
